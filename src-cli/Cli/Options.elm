d250a746b0af3082b02b8dd08abcf404c151ebf1
pravdomil <2387356+pravdomil@users.noreply.github.com>
2021-07-29 15:15:27 +0200

    Cli.Options

---
+++ src-cli/Cli/Options.elm
@@ -0,0 +1,60 @@
+module Cli.Options exposing (..)
+
+import Parser as P exposing ((|.), (|=), Parser)
+
+
+type alias Options =
+    { shebang : Bool
+    , autorun : Bool
+    , files : List String
+    }
+
+
+
+--
+
+
+parse : List String -> Result (List P.DeadEnd) Options
+parse a =
+    a |> String.join "\u{0000}" |> P.run parser
+
+
+parser : Parser Options
+parser =
+    let
+        loop : Options -> Parser (P.Step Options Options)
+        loop acc =
+            P.oneOf
+                [ P.succeed (\v -> P.Loop { acc | shebang = v })
+                    |= boolArg "shebang"
+                , P.succeed (\v -> P.Loop { acc | autorun = v })
+                    |= boolArg "autorun"
+
+                --
+                , P.succeed (\v -> P.Loop { acc | files = v :: acc.files })
+                    |= P.getChompedString (P.chompUntilEndOr "\u{0000}")
+                    |. P.oneOf
+                        [ P.symbol "\u{0000}"
+                        , P.end
+                        ]
+
+                --
+                , P.succeed (\_ -> P.Done { acc | files = List.reverse acc.files })
+                    |= P.end
+                ]
+
+        boolArg : String -> Parser Bool
+        boolArg name =
+            P.succeed True
+                |. P.symbol ("--" ++ name)
+                |. P.oneOf
+                    [ P.symbol "\u{0000}"
+                    , P.end
+                    ]
+    in
+    P.loop
+        { shebang = False
+        , autorun = False
+        , files = []
+        }
+        loop
