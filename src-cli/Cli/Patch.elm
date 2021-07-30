c33a57e9fe580c5f39728824f41d214f70b8134a
pravdomil <2387356+pravdomil@users.noreply.github.com>
2021-07-30 17:06:05 +0200

    format

--- src-cli/Cli/Patch.elm
+++ src-cli/Cli/Patch.elm
@@ -3,17 +3,16 @@ module Cli.Patch exposing (..)
 
 function : String
 function =
-    """
-    function(name) {
-      var fn = new Function(['args'], 'return ' + name + '.apply(' + name + ', args)')
+    """function(name) {
+         var fn = new Function(['args'], 'return ' + name + '.apply(' + name + ', args)')
 
-      return $author$project$Interop$JavaScript$Function(function(args) {
-        return _Scheduler_binding(function(callback) {
-          var result;
-          try       { result = _Scheduler_succeed(_Json_wrap(fn(args))) }
-          catch (e) { result = _Scheduler_fail   ($author$project$Interop$JavaScript$Exception(_Json_wrap(e))) }
-          callback(result)
-        })
-      })
-    }
+         return $author$project$Interop$JavaScript$Function(function(args) {
+           return _Scheduler_binding(function(callback) {
+             var result;
+             try       { result = _Scheduler_succeed(_Json_wrap(fn(args))) }
+             catch (e) { result = _Scheduler_fail   ($author$project$Interop$JavaScript$Exception(_Json_wrap(e))) }
+             callback(result)
+           })
+         })
+       };
     """
