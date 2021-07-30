afd2b5e9583c709b201d22b2feba767d87aff3c5
pravdomil <2387356+pravdomil@users.noreply.github.com>
2021-07-30 16:57:56 +0200

    Cli.Patch.function

---
+++ src-cli/Cli/Patch.elm
@@ -0,0 +1,19 @@
+module Cli.Patch exposing (..)
+
+
+function : String
+function =
+    """
+    function(name) {
+      var fn = new Function(['args'], 'return ' + name + '.apply(' + name + ', args)')
+
+      return $author$project$Interop$JavaScript$Function(function(args) {
+        return _Scheduler_binding(function(callback) {
+          var result;
+          try       { result = _Scheduler_succeed(_Json_wrap(fn(args))) }
+          catch (e) { result = _Scheduler_fail   ($author$project$Interop$JavaScript$Exception(_Json_wrap(e))) }
+          callback(result)
+        })
+      })
+    }
+    """
