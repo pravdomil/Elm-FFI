3efc4a782bd2b87842baaf136fd1c5dd22b0dbd9
pravdomil <2387356+pravdomil@users.noreply.github.com>
2021-07-30 17:13:54 +0200

    Cli.Patch.asyncFn

--- src-cli/Cli/Patch.elm
+++ src-cli/Cli/Patch.elm
@@ -16,3 +16,20 @@ function =
          })
        };
     """
+
+
+asyncFunction : String
+asyncFunction =
+    """function(name) {
+         var fn = new Function(['args'], 'return ' + name + '.apply(' + name + ', args)')
+
+         return $author$project$Interop$JavaScript$Function(function(args) {
+           return _Scheduler_binding(async function(callback) {
+             var result;
+             try       { result = _Scheduler_succeed(_Json_wrap(await fn(args))) }
+             catch (e) { result = _Scheduler_fail   ($author$project$Interop$JavaScript$Exception(_Json_wrap(e))) }
+             callback(result)
+           })
+         })
+       };
+    """
