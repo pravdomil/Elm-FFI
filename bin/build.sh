1dc5f6abd48581c71115fb1d8fa8983d4e0a114b
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-09-30 20:41:56 +0200

    next

--- bin/build.sh
+++ bin/build.sh
@@ -9,6 +9,10 @@ set -u
 # Be in project root.
 cd "${0%/*}/.."
 
+# Install dependencies from npm.
+npm i
+export PATH="$PWD/node_modules/.bin:$PATH"
+
 # Compile application.
 elm make src-cli/Main.elm --output bin/elm-ffi.js --optimize
 elm-ffi bin/elm-ffi.js --run --shebang
